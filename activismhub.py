1d4ac7247d6cf930f5a589de6fb75b4b36c8efd8
manyam686 <61163106+manyam686@users.noreply.github.com>
2021-04-08 22:34:29 -0700

    Added email verification and password resets

--- activismhub.py
+++ activismhub.py
@@ -10,8 +10,11 @@
 
 from flask import Flask, request, render_template, session, redirect
 from flask_mysqldb import MySQL
-import os, datetime
+import os, datetime, secrets
 from werkzeug.utils import secure_filename
+import smtplib, ssl, hashlib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 #INSTANTIATE
 app = Flask(__name__)
@@ -43,6 +46,23 @@ CLUB_TABLE='testClub'
 EVENT_TABLE='testClub_event'
 ADMIN_TABLE='testClub_admin'
 
+#salt for password hashing
+salt = '1kn0wy0ulov3m3'
+
+
+
+def activeType():
+    cursor = mysql.connection.cursor()
+    cursor.execute('''SELECT club_email, email_activated FROM %s'''%(CLUB_TABLE,))
+    results = cursor.fetchall()
+    for result in results:
+        print(result['club_email'])
+        print(result['email_activated'])
+    cursor.execute('''SELECT clubID FROM %s WHERE club_email="manyam686@gmail.com"'''%(CLUB_TABLE,))
+    me = len(cursor.fetchall())
+    print("Num my email: ",me)
+
+
 ########################################################################################################################
 ##### Main pages #######################################################################################################
 
@@ -64,6 +84,8 @@ def index():
         event['event_date_formatted']=formatDateFromSql(event['event_date'])
         event['start_time_formatted']=formatTimeFromSql(event['start_time'])
         event['end_time_formatted']=formatTimeFromSql(event['end_time'])
+   #render homepage
+   activeType()
    return render_template("homePage.html",events=events,clubs=clubs)
 
 
@@ -115,18 +137,26 @@ def do_login():
    user = request.form['Email']
    password = request.form['Password']
    #check if club email exists
-   cursor.execute('''SELECT EXISTS(SELECT * FROM %s WHERE club_email = %%s) AS club_exists'''%(CLUB_TABLE,),(user,))
-   if cursor.fetchall()[0]['club_exists']==1:
+   cursor.execute('''SELECT clubID, password, email_activated FROM %s WHERE club_email = %%s'''%(CLUB_TABLE,),(user,))
+   result = cursor.fetchall()
+   if len(result)==1:
        #get club ID and correct password
-       cursor.execute('''SELECT clubID, password FROM %s where club_email = %%s'''%(CLUB_TABLE,),(user,))
-       result=cursor.fetchall()[0]
+       result = result[0]
        clubID = result['clubID']
        correct_password = result['password']
-       #simple authentication
+       active = result['email_activated']
+       #hash inputted password
+       saltedPassword = password + salt
+       password = hashlib.sha256(saltedPassword.encode()).hexdigest()
+       #authentication
        if password == correct_password:
-           #add clubID to session and reroute to homepage
-           session['club_id']=clubID
-           return index()
+           if active ==1:
+               #add clubID to session and reroute to homepage
+               session['club_id']=clubID
+               return index()
+           else:
+               #if club not active, reload login page
+               return login_page("Email not verified. Check your email for a verification link.")
        else:
            #if password incorrect, reload login page
            return login_page("Incorrect password.")
@@ -160,24 +190,40 @@ def enter_account():
    #instantiate cursor
    cursor = mysql.connection.cursor()
    #get form info
-   email = request.form['clubEmail']
-   clubName = request.form['club-name']
-   adminName = request.form['admin-name']
-   description = request.form['club-description']
-   adminEmail = request.form['admin-email']
+   club_email = request.form['clubEmail']
+   club_name = request.form['club-name']
+   admin_name = request.form['admin-name']
+   about_info = request.form['club-description']
+   admin_email = request.form['admin-email']
    password = request.form['password']
    club_email_display = request.form.get('club_email_display') != None
+   #if email already associated with a club, display error and return
+   cursor.execute('''SELECT clubID FROM %s WHERE club_email=%%s'''%(CLUB_TABLE,),(club_email,))
+   emailExists = len(cursor.fetchall())>0
+   #TODO - add message that email is already taken - popup?
+   if emailExists:
+        return create_account()
+   #hash password
+   saltedPassword = password + salt
+   password = hashlib.sha256(saltedPassword.encode()).hexdigest()
+   #Create activation hash
+   activation_hash = secrets.token_urlsafe()
    #Insert new account info into club table
-   cursor.execute('''INSERT INTO %s(club_name,about_info,club_email,password,club_email_display)
-       VALUES(%%s,%%s,%%s,%%s,%%s)'''%(CLUB_TABLE,),(clubName,description,email,password,club_email_display))
+   cursor.execute('''INSERT INTO %s(club_name,about_info,club_email,password,club_email_display,activation_hash,
+       email_activated) VALUES(%%s,%%s,%%s,%%s,%%s,%%s,0)'''%(CLUB_TABLE,),(club_name,about_info,club_email,password,
+       club_email_display,activation_hash))
    mysql.connection.commit()
    #get club new club id
-   cursor.execute('''SELECT clubID FROM %s where club_name = %%s'''%(CLUB_TABLE,),(clubName,))
+   cursor.execute('''SELECT clubID FROM %s where club_name = %%s'''%(CLUB_TABLE,),(club_name,))
    clubID=cursor.fetchall()[0]['clubID']
    #Insert new account info into admin table
    cursor.execute('''INSERT INTO %s(clubID,admin_name,admin_email) VALUES(%%s,%%s,%%s)'''%(ADMIN_TABLE,),
-        (clubID,adminName,adminEmail))
+        (clubID,admin_name,admin_email))
    mysql.connection.commit()
+   #Create text for email verification and send email
+   texts = verifyEmailText(club_email,activation_hash)
+   sendEmail(club_email,texts['html'],texts['text'],"Verify your email")
+   #TODO - render page with message about sent email
    #reroute to home page
    return index()
 
@@ -372,7 +418,7 @@ def updateEvent():
 #Gets list of club names and IDs
 def getClubs():
    cursor = mysql.connection.cursor()
-   cursor.execute('''SELECT club_name, clubID FROM %s''' % (CLUB_TABLE,))
+   cursor.execute('''SELECT club_name, clubID FROM %s WHERE email_activated=1''' % (CLUB_TABLE,))
    return cursor.fetchall()
 
 
@@ -397,5 +443,201 @@ def formatTimeFromSql(time):
         hours=hours-12
         ampm='PM'
     return str(hours)+":"+str(min)+" "+ampm
+
+########################################################################################################################
+##########General Email Functions#######################################################################################
+
+#Sends an email with the given html and plain text messages and subject to the given email
+#from activismhub@gmail.com
+def sendEmail(receiver_email,html_text,plain_text,subject):
+    sender_email='activismhub@gmail.com'
+    password = 'cscap7285!' #Need to figure out how to do this more securely
+
+    message = MIMEMultipart('alternative')
+    message['Subject']=subject
+    message['From']=sender_email
+    message['To']=receiver_email
+
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(plain_text, "plain")
+    part2 = MIMEText(html_text, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part1) #plain text version, email client will render if html fails
+    message.attach(part2) #html version, email client will try to render first
+
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
+
+
+########################################################################################################################
+##########Verifying Emails##############################################################################################
+
+#Returns a dict with html and plain versions of a verify password email body
+def verifyEmailText(email,hash):
+    link=f"http://localhost:5000/verifyEmail?e={email}&h={hash}"
+    html=f"""\
+        <html>
+          <body>
+            <p>Hello,<br><br>
+               Welcome to ACTivism Hub!<br>
+               Please verify your email by clicking <a href={link}>here</a>.<br><br>
+               Best,<br>
+               The ACTivism Hub Team
+            </p>
+          </body>
+        </html>
+        """
+    text = f"""\
+        Hello,
+
+        Welcome to ACTivism Hub!
+        Please verify your email by clicking the link below.
+
+        Best,
+        The ACTivism Hub Team
+
+        {link}
+        """
+    return {'html':html,'text':text}
+
+
+@app.route("/verifyEmail")
+def verifyEmail():
+    #email and hash from url
+    club_email = request.args.get("e")
+    hash = request.args.get("h")
+    #get clubID and hash associated with this email from database
+    cursor = mysql.connection.cursor()
+    #NOTE - is it possible there would be none/more than one of this email in the db
+    cursor.execute('''SELECT clubID, activation_hash, email_activated FROM %s WHERE club_email=%%s'''%(CLUB_TABLE,),(club_email,))
+    r = cursor.fetchall()
+    print(len(r))
+    print(club_email)
+    print(type(club_email))
+    results = r[0]
+    clubID=results['clubID']
+    db_hash = results['activation_hash']
+    active = results['email_activated']
+    #check if hash matches what we have stored for this email and account isn't active
+    if active == 1:
+        return login_page("Club has already been activated.")
+    if db_hash != hash:
+        message="Email verification failed."
+        #how to reroute here?? With popup
+    #activate club
+    cursor.execute('''UPDATE %s SET email_activated=1 WHERE clubID=%%s'''%(CLUB_TABLE,),(clubID,))
+    mysql.connection.commit()
+    #reroute to login page
+    return login_page("Email verification successful.")
+
+
+########################################################################################################################
+##########Resetting Passwords###########################################################################################
+
+#When click forgot password on login page
+@app.route("/forgotPassword")
+def forgotPassword():
+    clubs = getClubs()
+    return render_template("forgotPassword.html",clubs=clubs)
+
+
+#When click reset password button in forgot password page
+@app.route("/preparePasswordReset",methods=["POST"])
+def preparePasswordReset():
+   #get email from form
+   club_email=request.form['club_email']
+   #Get clubID for that email
+   cursor = mysql.connection.cursor()
+   cursor.execute('''SELECT clubID, activation_hash FROM %s WHERE club_email=%%s'''%(CLUB_TABLE,),(club_email,))
+   result = cursor.fetchall()
+   #if that email isn't in the database
+   #TODO - add message saying email not in database
+   if len(result) == 0:
+        return forgotPassword()
+   #Prepare text and send email
+   clubID=result[0]['clubID']
+   activation_hash = result[0]['activation_hash']
+   texts = resetPasswordText(club_email,activation_hash)
+   sendEmail(club_email,texts['html'],texts['text'],"Reset your password")
+   #TODO - where to return when email sent? Add pop up
+   return index()
+
+
+#route for link clicked in email
+@app.route("/resetPassword")
+def resetPassword():
+    #email and hash from url
+    club_email = request.args.get("e")
+    hash = request.args.get("h")
+    #get clubID and hash associated with this email from database
+    cursor = mysql.connection.cursor()
+    #NOTE - is it possible there would be none/more than one of this email in the db
+    cursor.execute('''SELECT clubID, activation_hash from %s WHERE club_email=%%s'''%(CLUB_TABLE,),(club_email,))
+    results = cursor.fetchall()[0]
+    clubID=results['clubID']
+    db_hash = results['activation_hash']
+    #check if hash matches what we have stored for this email and account is active
+    #if passes, render a reset password page, pass in clubID
+    if db_hash == hash:
+        clubs = getClubs()
+        return render_template("resetPassword.html",clubs=clubs,clubID=clubID)
+    else:
+        return login_page("Reset password via email failed")
+
+
+#When click submit on reset password form
+@app.route("/doPasswordReset",methods=["POST"])
+def doPasswordReset():
+    #get info from form
+    password = request.form['password']
+    clubID = request.form['clubID']
+    #hash password
+    saltedPassword = password + salt
+    password = hashlib.sha256(saltedPassword.encode()).hexdigest()
+    #update password in database
+    cursor = mysql.connection.cursor()
+    cursor.execute('''UPDATE %s SET password=%%s WHERE clubID=%%s'''%(CLUB_TABLE,),(password,clubID))
+    mysql.connection.commit()
+    #render login page
+    return login_page("Password successfully reset.")
+
+
+#returns a dict with the html and plain text versions of a reset password email
+def resetPasswordText(email,hash):
+    link=f"http://localhost:5000/resetPassword?e={email}&h={hash}"
+    html=f"""\
+        <html>
+          <body>
+            <p>Hello,<br><br>
+               If you are not trying to reset your password on ACTivism Hub, ignore this email.<br>
+               Otherwise, reset your password <a href={link}>here</a>.<br><br>
+               Best,<br>
+               The ACTivism Hub Team
+            </p>
+          </body>
+        </html>
+        """
+    text = f"""\
+        Hello,
+
+        If you are not trying to reset your password on ACTivism Hub, ignore this email.
+        Otherwise, reset your password by clicking the link below.
+
+        Best,
+        The ACTivism Hub Team
+
+        {link}
+        """
+    return {'html':html,'text':text}
+
+
+
 
 
